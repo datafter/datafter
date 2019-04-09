8e84cdd0867474058d80d809f12dcec331afd742
Mélissa Loeser <melissa.loeser@gmail.com>
2019-04-09 22:59:55 +0200

    current auth: add field first_name, last_name and date_of_birth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+=begin
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+=end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :is_female, :date_of_birth) }
+  end
 end
